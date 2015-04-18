f5ab3e1e8c775bb7ccee64b260e3fb393f82bce7
Coni Cosma <cristina.cosma@fortech.ro>
2015-04-18 10:47:47 +0300

    Code cleanup. Trained data is successfully copied from assets to device storage.

--- app/src/main/java/ro/utcn/foodapp/ocr/OcrInitAsyncTask.java
+++ app/src/main/java/ro/utcn/foodapp/ocr/OcrInitAsyncTask.java
@@ -14,6 +14,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 import ro.utcn.foodapp.camera.PreviewPhotoActivity;
+import ro.utcn.foodapp.utils.Constants;
 
 /**
  * Created by coponipi on 17.04.2015.
@@ -29,7 +30,7 @@ public class OcrInitAsyncTask extends AsyncTask<Void, Void, Void> {
         this.previewPhotoActivity = previewPhotoActivity;
         this.tessBaseAPI = tessBaseAPI;
         this.bitmap = bitmap;
-        this.language = "ron";
+        this.language = Constants.OCR_TRAINED_DATA_LANGUAGE;
 
     }
 
@@ -39,33 +40,22 @@ public class OcrInitAsyncTask extends AsyncTask<Void, Void, Void> {
 
     }
 
-    private void copyFile(InputStream in, OutputStream out) {
-        byte[] buffer = new byte[1024];
-        int read;
-        try {
-            while ((read = in.read(buffer)) != -1) {
-                out.write(buffer, 0, read);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     @Override
     protected Void doInBackground(Void... params) {
-        AssetManager assetManager = previewPhotoActivity.getAssets();
-        String destinationPath = Environment.getExternalStorageDirectory() + "/tesseract/";
-        InputStream in = null;
-        OutputStream out = null;
-        if (!new File(destinationPath).exists()) {
+        String destinationPath = Environment.getExternalStorageDirectory() + File.separator + "tesseract" + File.separator;
+
+        // If traineddata file does not exists in tesseract/tessdata, copy it from assets to device storage
+        if (!new File(destinationPath + File.separator + "tessdata" + File.separator + "ron.traineddata").exists()) {
+            AssetManager assetManager = previewPhotoActivity.getAssets();
+            InputStream in = null;
+            OutputStream out = null;
             try {
-                in = assetManager.open("tesseract/tessdata/ron.traineddata");
-                File outFile = new File(destinationPath+"/tessdata/");
+                in = assetManager.open("tesseract" + File.separator + "tessdata" + File.separator + "ron.traineddata");
+                File outFile = new File(destinationPath + File.separator + "tessdata" + File.separator);
                 if (!outFile.exists())
                     outFile.mkdirs();
-                //datapath + File.separator + "tessdata" + File.separator + "ron" + ".traineddata"
-
-                out = new FileOutputStream(outFile + "/ron.traineddata");
+                out = new FileOutputStream(outFile + File.separator + "ron.traineddata");
 
                 copyFile(in, out);
                 in.close();
@@ -74,16 +64,15 @@ public class OcrInitAsyncTask extends AsyncTask<Void, Void, Void> {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-//        File dir = new File(datapath + "tessdata/");
-//
-
         }
         tessBaseAPI = new TessBaseAPI();
         tessBaseAPI.setDebug(true);
 
-
+        // Init the tesseract engine with the path of traineddata and the used language
         tessBaseAPI.init(destinationPath, language);
+        // Send the image to tesseract engine to perform ocr
         tessBaseAPI.setImage(bitmap);
+        // Get the recognized text from image
         recognizedText = tessBaseAPI.getUTF8Text();
 
         return null;
@@ -96,4 +85,15 @@ public class OcrInitAsyncTask extends AsyncTask<Void, Void, Void> {
         tessBaseAPI.end();
     }
 
+    private void copyFile(InputStream in, OutputStream out) {
+        byte[] buffer = new byte[2048];
+        int read;
+        try {
+            while ((read = in.read(buffer)) != -1) {
+                out.write(buffer, 0, read);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ app/src/main/java/ro/utcn/foodapp/utils/Constants.java
@@ -0,0 +1,8 @@
+package ro.utcn.foodapp.utils;
+
+/**
+ * Created by coponipi on 18.04.2015.
+ */
+public class Constants {
+    public static final String OCR_TRAINED_DATA_LANGUAGE = "ron";
+}
