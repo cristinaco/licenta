c0dc18c1dfc234278f30f55c624e64c05e2e97d7
Coni Cosma <cristina.cosma@fortech.ro>
2015-04-19 19:20:26 +0300

    Added model class Product.

---
+++ app/src/main/java/ro/utcn/foodapp/model/Product.java
@@ -0,0 +1,63 @@
+package ro.utcn.foodapp.model;
+
+import java.util.Date;
+
+/**
+ * Created by coponipi on 19.04.2015.
+ */
+public class Product {
+    private String uid;
+    private String name;
+    private String description;
+    private int piecesNumber;
+    private Date expirationDate;
+    private String expirationStatus;
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPiecesNumber() {
+        return piecesNumber;
+    }
+
+    public void setPiecesNumber(int piecesNumber) {
+        this.piecesNumber = piecesNumber;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public String getExpirationStatus() {
+        return expirationStatus;
+    }
+
+    public void setExpirationStatus(String expirationStatus) {
+        this.expirationStatus = expirationStatus;
+    }
+}
